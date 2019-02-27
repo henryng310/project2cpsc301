b123e4e6117e3c0b18438e395de045f7882d15c2
henryng310 <henryng310@yahoo.com>
2019-02-26 17:04:18 -0800

    Create person.cpp

---
+++ person.cpp
@@ -0,0 +1,42 @@
+#include "person.h"
+
+
+string Person::getFirstName() {
+	return firstName;
+}
+
+void Person::setLastName(string lName) {
+	myFile >> lastName;
+}
+
+string Person::getLastName() {
+	return lastName;
+}
+
+void setFirstName(string fName) {
+
+}
+
+void setPayRate(float rate) {
+
+}
+
+float getPayRate() {
+	return payRate;
+}
+
+void setHoursWorked(float hours) {
+
+}
+
+float getHoursWorked() {
+	return hoursWorked;
+}
+
+float totalPay() {
+
+}
+ 
+string fullName() {
+
+}
