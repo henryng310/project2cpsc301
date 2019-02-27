1905a913d8b53d70d42ef2d338c4d4ccb422ef84
henryng310 <henryng310@yahoo.com>
2019-02-26 17:05:15 -0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include "person.h"
+
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <fstream>
+
+using namespace std;
+
+
+
+int main() {
+
+	fstream myFile;
+	myFile.open("input.txt");
+
+	
+
+
+
+
+
+	system("pause");
+	return 0;
+}
